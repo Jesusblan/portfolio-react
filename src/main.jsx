a75e5d66ee768076375e151f736c49172c5a7cb1
JesusBlan <jezublanco@gmail.com>
2025-03-09 12:58:47 +0100

    rediseño completo de la web, empezamos de 0

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Header from "./components/Header.jsx";
-import Menu from "./components/Menu.jsx";
+
 
 createRoot(document.getElementById("root")).render(
   <>
-    <Header />
-    <Menu />
+
   </>,
 );
