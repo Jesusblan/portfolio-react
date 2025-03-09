ec96ae907b93506d57b5d4fb0b6234aba0588546
JesusBlan <jezublanco@gmail.com>
2025-03-09 13:57:47 +0100

    nuevo modulo para el header con un icono para abrir el menu y otro para redireccionar a mi github

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,28 @@
-import "../styles/Header.css"
-import Icon from '@mdi/react';
-import { mdiMenu } from '@mdi/js';
+import "../styles/Header.css";
+import Icon from "@mdi/react";
+import { mdiMenu } from "@mdi/js";
+import { mdiGithub } from "@mdi/js";
 
+export default function Header() {
+  function gitClick() {
+    window.location.href = "https://github.com/Jesusblan";
+  }
 
-
-export default function Header(){
-
+  function dropMenu(){
     return (
-        <div className="header">
-            <Icon path={mdiMenu} size={1} />
-            <h2>test</h2>
+        <div className="dropMenu">
+            <button>Home</button>
+            <button>Proyectos</button>
+            <button>CV</button>
         </div>
     );
-}
+  }
 
+  return (
+    <div className="header">
+      <Icon path={mdiMenu} size={2} />
+      <br />
+      <Icon onClick={gitClick} path={mdiGithub} size={2} />
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Header from "./components/Header";
 
-
 createRoot(document.getElementById("root")).render(
   <>
     <Header />
-
   </>,
 );
