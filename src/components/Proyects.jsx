fb4d98821a4058a9d4c36b62472cc71349119156
JesusBlan <jezublanco@gmail.com>
2025-03-10 21:14:23 +0100

    nuevo componente para la seccion de proyectos

---
+++ src/components/Proyects.jsx
@@ -0,0 +1,10 @@
+import "../styles/Proyects.css"
+
+export default function Proyects(){
+
+    return (
+        <div className="proyectos">
+            <h3>Proyectos</h3>
+        </div>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Header from "./components/Header";
 import Info from "./components/Info";
+import Proyects from "./components/Proyects";
 
 createRoot(document.getElementById("root")).render(
   <>
     <Header />
     <Info />
+    <Proyects />
   </>,
 );
