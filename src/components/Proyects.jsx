560880518dce1c5821f660453c59600cf4da65f6
JesusBlan <jezublanco@gmail.com>
2025-03-11 20:55:49 +0100

    cambio de paleta de colores

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -5,6 +5,10 @@ export default function Proyects(){
     return (
         <div className="proyectos">
             <h3>Proyectos</h3>
+            <a href="">C.V Aplication</a>
+            <a href="">Wheater app</a>
+            <a href="">Restaurant page</a>
+            <a href="">Image Carousel</a>
         </div>
     );
 }
