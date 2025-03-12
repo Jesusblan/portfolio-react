681fdfee0be4a3ec5d418203e3588e6509dc07e5
JesusBlan <jezublanco@gmail.com>
2025-03-12 15:27:34 +0100

    aplicar prettier

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,8 +1,6 @@
 import "../styles/Info.css";
 
 export default function Info() {
-
-    
   function gitClick() {
     window.location.href = "https://github.com/Jesusblan";
   }
@@ -11,7 +9,10 @@ export default function Info() {
     <div className="info">
       <h1>Jesús Blanco</h1>
       <p>ReactJS - JavaScript - CSS - HTML</p>
-      <p>Lorem ipsilum relleno wapo ahora me pienso que piner blabralbralbr seguro que se ma hace largo al final yatusabe bjabajabja eso mu bien</p>
+      <p>
+        Lorem ipsilum relleno wapo ahora me pienso que piner blabralbralbr
+        seguro que se ma hace largo al final yatusabe bjabajabja eso mu bien
+      </p>
       <p className="git" onClick={gitClick}>
         Echale un vistazo a mi GitHub!
       </p>
