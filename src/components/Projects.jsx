d3eda47916440d686411458db7b7096e8d2077cf
JesusBlan <jezublanco@gmail.com>
2025-03-14 23:19:19 +0100

    un poco de prettier

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,10 +4,35 @@ export default function Projects() {
   return (
     <div className="proyectos">
       <h2>Proyectos</h2>
-      <p onClick={() => window.location.href="https://cv-react-jesusblanco.netlify.app"}>C.V Aplication</p>
-      <p onClick={() => window.location.href="https://github.com/Jesusblan/weather-app"}>Wheater app</p>
-      <p onClick={() => window.location.href="https://jesusblan.github.io/restaurant-js/"}>Restaurant page</p>
-      <p onClick={() => window.location.href="https://github.com/Jesusblan/imageCarousel-dropdownMenus-js"}>Image Carousel</p>
+      <p
+        onClick={() =>
+          (window.location.href = "https://cv-react-jesusblanco.netlify.app")
+        }
+      >
+        C.V Aplication
+      </p>
+      <p
+        onClick={() =>
+          (window.location.href = "https://github.com/Jesusblan/weather-app")
+        }
+      >
+        Wheater app
+      </p>
+      <p
+        onClick={() =>
+          (window.location.href = "https://jesusblan.github.io/restaurant-js/")
+        }
+      >
+        Restaurant page
+      </p>
+      <p
+        onClick={() =>
+          (window.location.href =
+            "https://github.com/Jesusblan/imageCarousel-dropdownMenus-js")
+        }
+      >
+        Image Carousel
+      </p>
     </div>
   );
 }
